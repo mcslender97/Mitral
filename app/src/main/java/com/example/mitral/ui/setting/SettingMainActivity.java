a4c1b361d9a49f0c9c337198890adb4a7857fdb7
Kurt <mcslender97@gmail.com>
2020-04-21 23:09:55 -0400

    Added button and Onclick listener/action to switch from Settings to Main

--- app/src/main/java/com/example/mitral/ui/setting/SettingMainActivity.java
+++ app/src/main/java/com/example/mitral/ui/setting/SettingMainActivity.java
@@ -1,7 +1,28 @@
 package com.example.mitral.ui.setting;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mitral.R;
+import com.example.mitral.ui.main.MainActivity;
 
 public class SettingMainActivity extends AppCompatActivity {
     private SettingViewModel settingViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonOne = findViewById(R.id.confirm_changes);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+                Intent confirmSettingChanges = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(confirmSettingChanges);
+            }
+        });
+    }
 }
